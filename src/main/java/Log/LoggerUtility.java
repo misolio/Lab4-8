3202cdeed5e3bff59cf8b1bc6b1c6dd04fe176e4
Solomia <solomiia.oliinyk.oi.2023@lpnu.ua>
2024-11-29 13:59:30 +0200

    Лаба 5 - 8

---
+++ src/main/java/Log/LoggerUtility.java
@@ -0,0 +1,25 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggerUtility {
+    private static final Logger logger = LoggerFactory.getLogger(LoggerUtility.class);
+
+    private LoggerUtility() {
+    }
+
+    public static void logInfo(String message) {
+        logger.info(message);
+    }
+
+    public static void logWarning(String message) {
+        logger.warn(message);
+    }
+
+    public static void logError(String message, Throwable throwable) {
+        logger.error(message, throwable);
+    }
+
+    public static void logDebug(String message) {
+        logger.debug(message);
+    }
+}
